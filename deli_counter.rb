cfb446f09df3708fefd4d467b1b0d1f70d1f7336
Dametrik Fick <dametrik.fick@gmail.com>
2020-01-31 23:39:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,6 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else katz_deli.any?
-    puts "The line is currently:"
+    puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
   end
 end
